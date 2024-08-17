1c79ad0ebfbab28b075c4b3ad8a3c321727e58c7
Mainuddin Alam Irteja <mainuddinalamirteja@gmail.com>
2024-08-17 11:32:43 -0300

    Added the prompt input function

--- term_Music_Player.rb
+++ term_Music_Player.rb
@@ -1,1 +1,16 @@
-puts "hello world"
+# Ruby script to play music in the terminal
+# Author: Mainuddin Alam Irteja
+
+"""
+Function to prompt user to input song name
+
+@returns songName The name of the song with the artist
+"""
+def promptInput()
+   print "Please give the name of the song and artist: "
+   songName = gets.chomp
+   return songName
+end
+
+sName = promptInput()
+puts "THe song name is: #{sName}"
