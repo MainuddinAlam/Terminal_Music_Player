68cbeb454099ef80da4c88549b2585839e2b3292
Mainuddin Alam Irteja <mainuddinalamirteja@gmail.com>
2024-08-18 22:31:12 -0300

    Planning out functions

--- term_Music_Player.rb
+++ term_Music_Player.rb
@@ -12,5 +12,19 @@ def promptInput()
    return songName
 end
 
+"""
+"""
+def getMusic()
+
+end
+
+"""
+"""
+def musicControls()
+
+end
+
+
+# Get the song name
 sName = promptInput()
 puts "THe song name is: #{sName}"
